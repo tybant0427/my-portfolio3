96ffdf1bae31ef43f1bc32c1b9a8b3402acd649e
Tyler Bantley <tyler.bantley@icloud.com>
2022-04-07 21:08:05 -0500

    some component building

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Recommendations from './components/recommendations/Recommendations'
+import Portfolio from './components/portfolio/Portfolio'
+import Footer from './components/footer/Footer'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Services from './components/services/Services'
+import Experience  from './components/experience/Experience'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About /> 
+      <Experience />
+      <Services /> 
+      <Portfolio />
+      <Recommendations />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
