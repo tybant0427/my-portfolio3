82331391274d21bbaff32df87d754e71d37e9afb
Tyler Bantley <tyler.bantley@icloud.com>
2022-04-07 20:52:00 -0500

    file structure

---
+++ src/components/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <div>A</div>
+  )
+}
+
+export default About;
---
+++ src/components/recommendations/Recommendations.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './recommendations.css'
+
+const Recommendations = () => {
+  return (
+    <div>Recommendations</div>
+  )
+}
+
+export default Recommendations
