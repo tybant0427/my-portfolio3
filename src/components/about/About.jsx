a6490067bcaaff1a5a88a1816c417cb6e0bb5904
Tyler Bantley <tyler.bantley@icloud.com>
2022-04-10 19:52:54 -0500

    some finishing touches

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,25 +19,7 @@ const About = () => {
         </div>
 
         <div className="about__content">
-          <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
-              <h5>Experience</h5>
-              {/* <small>3+ Years Working</small> */}
-            </article>
-
-            <article className='about__card'>
-              <FiUsers className='about__icon'/>
-              <h5>Clients</h5>
-              {/* <small>200+ Worldwide</small> */}
-            </article>
-
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
-              <h5>Projects</h5>
-              {/* <small>80+ Completed</small> */}
-            </article>
-          </div>
+          
 
           <p>
           Full stack engineer specializing in front-end web development with a creative eye when it comes to design and user interaction. 
